fd68a4680698554d6bfc11e6a8e74dff4b4e8410
elkhedewy-group <elkhedewy-group@DESKTOP-J2MQAT6>
2026-02-04 05:05:13 +0200

    Add project files.

---
+++ FirstApp/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace FirstApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+			int[] arr = new int[2];
+			Console.WriteLine("Number of small carpets: ");
+			arr[0] = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Number of large carpets: ");
+			arr[1] = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Pirce per small carpet: 25$");
+			Console.WriteLine("Pirce per large carpet: 35$");
+			int cost = arr[0] * 25 + arr[1] * 35;
+			double tax = cost * 0.06;
+			Console.WriteLine($"Cost : {cost}");
+			Console.WriteLine($"Tax : {tax}");
+			double total = cost + tax;
+			Console.WriteLine($"Total estimate: {total} ");
+			Console.WriteLine("This estamite is valid for 30 days");
+			int X = 10;
+			int Y = 20;
+			Console.WriteLine($"Equation: {X} + {Y} = {X + Y:C}");
+
+		}
+	}
+}
